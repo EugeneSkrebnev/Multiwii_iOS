61a18f1486ab3c135a9b54265ec8f46981a64cd0
Eugene <Skrebnevchik@gmail.com>
2013-05-26 03:31:37 +0300

    Initial Commit

---
+++ MultiWii/MWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MWAppDelegate.h
+//  MultiWii
+//
+//  Created by Eugene Skrebnev on 5/26/13.
+//  Copyright (c) 2013 EugeneSkrebnev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MultiWii/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MultiWii
+//
+//  Created by Eugene Skrebnev on 5/26/13.
+//  Copyright (c) 2013 EugeneSkrebnev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MWAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MWAppDelegate class]));
+    }
+}
